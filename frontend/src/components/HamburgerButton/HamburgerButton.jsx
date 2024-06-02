2919725bda04e77efc77e1cd74ba03b95ce641a2
FabrizioZeballos <zeballosfab@gmail.com>
2024-06-02 00:33:42 -0300

    Create Menu

--- frontend/src/components/HamburgerButton/HamburgerButton.jsx
+++ frontend/src/components/HamburgerButton/HamburgerButton.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./HamburgerButton.module.css";
 import styles from "./HamburgerButton.module.css";
+import { Menu } from "../Menu/Menu";
 
 export const HamburgerButton = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -10,13 +11,15 @@ export const HamburgerButton = () => {
   };
 
   return (
-    <button
-      className={`${styles.hamburger} ${isMenuOpen && styles.open}`}
-      onClick={toggleMenu}
-    >
-      <span className={`${styles.bar} ${styles["bar-top"]}`}></span>
-      <span className={`${styles.bar} ${styles["bar-middle"]}`}></span>
-      <span className={`${styles.bar} ${styles["bar-bottom"]}`}></span>
-    </button>
+    <div>
+      <button className={`${styles.hamburger}`} onClick={toggleMenu}>
+        <span className={`${styles.bar} ${styles["bar-top"]}`}></span>
+        <span className={`${styles.bar} ${styles["bar-middle"]}`}></span>
+        <span className={`${styles.bar} ${styles["bar-bottom"]}`}></span>
+      </button>
+      <div className={styles["menu-container"]}>
+        {isMenuOpen && <Menu></Menu>}
+      </div>
+    </div>
   );
 };
